e437fa5b1717b1ad969ff65eea435040cf3746dd
pawelsawicz <pawel@sawicz.eu>
2014-06-22 13:46:21 +0200

    session resource add

---
+++ SpeakersAPI/Modules/SessionModule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace SpeakersAPI.Modules
+{
+    public class SessionModule : NancyModule 
+    {
+        public SessionModule()
+        {
+            Get["/sessions"] = parameter =>
+            {
+                return "some agenda data";
+            };
+
+            Get["/sessions/{id}"] = parameter =>
+            {
+                return "show session of id : " + parameter.id;
+            };           
+        }
+    }
+}
