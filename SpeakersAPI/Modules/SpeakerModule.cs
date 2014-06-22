c84b68ef05ddaebbb5baefde5afd035cb7b76e04
pawelsawicz <pawel@sawicz.eu>
2014-06-22 13:50:18 +0200

    SpeakerModule has been created

---
+++ SpeakersAPI/Modules/SpeakerModule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace SpeakersAPI.Modules
+{
+    public class SpeakerModule : NancyModule
+    {
+        public SpeakerModule()
+        {
+            Get["/speakers"] = parameter =>
+            {
+                return "some data of speakers";
+            };
+
+            Get["/speakers/{id}"] = parameter =>
+            {
+                return "some data of speker id : " + parameter.id;
+            };
+        }
+    }
+}
