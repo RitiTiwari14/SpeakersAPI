b764b2e49f8cb1583f910422191aa2d9cc22c354
pawelsawicz <pawel@sawicz.eu>
2014-06-22 13:33:18 +0200

    project init

---
+++ SpeakersAPI/Modules/HomeModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace SpeakersAPI.Modules
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ =>
+            {
+                return View["Index"];
+            };
+        }
+    }
+}
