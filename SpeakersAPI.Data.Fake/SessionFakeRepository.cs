5fd2366b522d2185314bccda934da4d7b2fd5967
pawelsawicz <pawel@sawicz.eu>
2014-07-13 18:47:09 +0200

    changed target framework version

---
+++ SpeakersAPI.Data.Fake/SessionFakeRepository.cs
@@ -0,0 +1,29 @@
+﻿using SpeakersAPI.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpeakersAPI.Data.Documents;
+
+namespace SpeakersAPI.Data.Fake
+{
+    public class SessionFakeRepository : ISessionRepository
+    {
+
+        public SessionDocument GetSessionByName(string nameOfDocument)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SessionDocument GetSessionById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<SessionDocument> GetAllSessions()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
